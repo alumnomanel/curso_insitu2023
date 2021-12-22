4122f909b31346858a8a25c7c36326c8b6955470
Manel <manelmontero009@gmail.com>
2021-12-22 11:46:33 +0100

    Primer commit proyecto nube

---
+++ fichero1.sh
@@ -0,0 +1,12 @@
+# esto se modifica en master
+crear_array(){
+ echo "inicio"
+ echo "esto lo hago en rama master"
+ echo "fin"
+}
+# Fichero fichero1.sh
+#esto lo incluimos en la rama2
+mostrar_array() {
+ echo"Ejemplo de funcion"
+ echo "fin"
+}
